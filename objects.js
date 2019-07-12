7426d125bb34c804eb3011de2bc4918b21a2b30e
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-12 11:18:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,7 @@
 var playlist = {"Freddie Gibbs": "Giannis"}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.update({}, playlist, {[artistName]: [songTitle]})
+  return Object.assign({}, playlist, {[artistName]: [songTitle]})
 }
+
+function 
