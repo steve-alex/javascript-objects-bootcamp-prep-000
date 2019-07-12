ee9d47d4ef1410dbb7c03f5c86c6cc219a332053
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-12 11:10:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,2 @@
 var playlist = {'Kanye west': "Devil in a new dress"};
 
-function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign(playlist, {[artistName]: [songTitle]})
-}
-
-function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName];
-  return playlist
-}
-
